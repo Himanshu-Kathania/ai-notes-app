f4fd8d2a48eabe307e8a7d6ca9e370e8599a5db5
Himanshu Kathania <hkworkrock@gmail.com>
2024-11-09 12:16:51 +0530

    project , backend , auth setup

---
+++ app/page.js
@@ -0,0 +1,47 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
+import React, { useEffect } from "react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+
+const Home = () => {
+  const { user } = useUser();
+  console.log({ user });
+  const createUser = useMutation(api.user.createUser);
+
+  useEffect(() => {
+    const CheckUser = async () => {
+      const result = await createUser({
+        email: user?.primaryEmailAddress?.emailAddress,
+        userName: user?.fullName,
+        imageUrl: user?.profileImageUrl,
+      });
+      console.log({ result });
+    };
+
+    if (user) {
+      CheckUser();
+    }
+  }, [createUser, user]);
+
+  return (
+    <div>
+      <h1>Hey</h1>
+      <Button>Like</Button>
+      <UserButton />
+    </div>
+  );
+};
+
+const App = () => (
+  <ConvexProvider client={convex}>
+    <Home />
+  </ConvexProvider>
+);
+
+export default App;
